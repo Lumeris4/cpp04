3596a1178fd2f930eccb392b1441e80faa29a871
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-02-25 12:26:05 +0100

    test

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 10:34:02 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/02/25 10:54:52 by lelanglo         ###   ########.fr       */
+/*   Updated: 2025/02/25 12:25:01 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 int main()
 {
+	//Animal failure;
 	Animal *tab[20];
 	for (int i = 0; i < 20; i++)
 	{
