47ff8adb0edaa9d53f15d4ce1ebd5185f6c43f6c
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-02-27 12:46:10 +0100

    oskour

--- ex02/Animal.hpp
+++ ex02/Animal.hpp
@@ -33,6 +33,6 @@ class Animal
 	public:
 	virtual ~Animal();
 	Animal &operator=(const Animal &other);
-	virtual void makeSound() const;
+	virtual void makeSound() const = 0;
 	std::string getType() const;
 };
