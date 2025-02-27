441b3a7b1c3a83b5351b010dbbff9d39be9a32d3
Leo Langlois <lelanglo@c2r10p4.42lehavre.fr>
2025-02-27 09:21:24 +0100

    push

--- ex02/Cat.cpp
+++ ex02/Cat.cpp
@@ -1,47 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Animal_utils.cpp                                   :+:      :+:    :+:   */
+/*   Cat.cpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lelanglo <lelanglo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/24 11:07:29 by lelanglo          #+#    #+#             */
-/*   Updated: 2025/02/25 10:28:48 by lelanglo         ###   ########.fr       */
+/*   Created: 2025/02/27 09:12:12 by lelanglo          #+#    #+#             */
+/*   Updated: 2025/02/27 09:13:35 by lelanglo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal_utils.hpp"
-
-Dog::Dog(): Animal()
-{
-	this->_Brain = new Brain;
-	Animal::_type = "Dog";
-	std::cout << "Dog constructor called\n";
-}
-
-Dog::Dog(const Dog &copy): Animal(copy)
-{
-	std::cout << "Dog constructor copy\n";
-	*this = copy;
-}
-
-Dog &Dog::operator=(const Dog &other)
-{
-	this->_type = other._type;
-	std::cout << "Operator Dog called\n";
-	return (*this);
-}
-
-Dog::~Dog()
-{
-	delete this->_Brain;
-	std::cout << KGRN "Destrucor Dog called\n" KNRM;
-}
-
-void Dog::makeSound() const
-{
-	std::cout << "OUAF OUAF\n";
-}
+#include "Cat.hpp"
 
 Cat::Cat(): Animal()
 {
